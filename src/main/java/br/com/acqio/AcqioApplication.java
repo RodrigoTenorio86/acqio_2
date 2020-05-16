1327931de1d909e281297a2e49e45ceeb76dc5dd
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2020-05-16 15:22:16 -0300

    first commit.

---
+++ src/main/java/br/com/acqio/AcqioApplication.java
@@ -0,0 +1,13 @@
+package br.com.acqio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcqioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcqioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/acqio/AcqioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acqio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcqioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
